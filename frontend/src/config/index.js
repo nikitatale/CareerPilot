4a6a5beec7655b02bcb3023e4e369774c38e0dfd
Nikita Tale <talenikita3@gmail.com>
2026-04-06 23:18:10 +0530

    Updated BASE_URL to deployed backend

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-export const BASE_URL = "http://localhost:8080"
+export const BASE_URL = "https://careerpilot-p9bi.onrender.com"
 
 export const clientServer = axios.create({
     baseURL: BASE_URL
