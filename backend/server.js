6bc8f3310533c0df2d1253d4c919464d3750c2bc
Nikita Tale <talenikita3@gmail.com>
2026-04-08 23:45:01 +0530

    Fix environment variable issue

--- backend/server.js
+++ backend/server.js
@@ -25,13 +25,13 @@ app.use(userRoutes);
 // app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+const PORT = process.env.PORT || 8080;
 
 const start = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB Connected");
 
-        const PORT = process.env.PORT || 8080;
 
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
